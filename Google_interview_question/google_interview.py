7b107561147024dbaaa9d5ddd6b463cb7bf1c423
wayne chan <waynestock666@gmail.com>
2024-06-28 01:25:55 +0100

    update

--- Google_interview_question/google_interview.py
+++ Google_interview_question/google_interview.py
@@ -1,3 +1,12 @@
+def bad_nested_solution(arr, target_value):
+	for i in range(len(arr)):
+		for j in range(i+1, len(arr)):
+			if arr[i] + arr[j] == target_value:
+				return "Yes"
+	return "NO"
+
+print(bad_nested_solution(arr1, tar))
+
 def has_pair_with_sum(arr, target_value):
 	complements = set()
 	
@@ -10,6 +19,10 @@ def has_pair_with_sum(arr, target_value):
 	
 arr1 = [1, 2, 3, 9]
 arr2 = [1, 2, 4, 4]
+
+print(bad_nested_solution(arr1, 8))
+print(bad_nested_solution(arr2, 8))
+
 
 print(has_pair_with_sum(arr1, 8))
 print(has_pair_with_sum(arr2, 8))
