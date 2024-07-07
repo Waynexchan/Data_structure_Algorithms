b8ab4cec93050b9518f4db3a66a246e234f1493f
wayne chan <waynestock666@gmail.com>
2024-07-07 02:49:46 +0100

    update

--- Algorithms/Dynamic Programming/Fibonacci_Dynamic.py
+++ Algorithms/Dynamic Programming/Fibonacci_Dynamic.py
@@ -26,6 +26,16 @@ def fibonacci_dynamic():
             
     return fib, get_calculation_count
 
+def fibonacci_dynamic_bottom_up(n):
+    answer = [0,1]
+    for i in range (3, len(n)):
+        answer.push(answer[i-1] + answer[i-2])
+
+    return answer[-1]
+
 fasterFib , get_calculation_count= fibonacci_dynamic()
 print(fasterFib(10))
 print(f'We did {get_calculation_count()} calculations')
+
+# O (n) Time vs O(2^n)
+# Drawback Increase Space Complexity.
